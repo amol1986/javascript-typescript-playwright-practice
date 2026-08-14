275d06c3e26747ea49c7032f11aa07363f08e16f
amol1986 <amolsureshpisal@gmail.com>
2026-08-14 15:27:55 +0530

    Practiced JavaScript data types

---
+++ 01-javascript-basics/02-variables.js
@@ -0,0 +1,7 @@
+let Name = "Amol";
+let Experience = 11;
+let LearningJavascript = true;
+
+console.log("Name:", Name);
+console.log("Experience:", Experience);
+console.log("Learning JavaScript:", LearningJavascript);
---
+++ 01-javascript-basics/03-data-types.js
@@ -0,0 +1,19 @@
+/* String
+Number
+Boolean
+Undefined
+Null
+Object */
+
+let name = "Amol";
+let age = 41;
+let maritalStatus = true;
+let isLogin;
+let password = null;
+let company = { name: "Trigyn", location: "Mumbai" };
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof mariatalStatus);
+console.log(typeof isLogin);
+console.log(typeof password);
+console.log(typeof company);
