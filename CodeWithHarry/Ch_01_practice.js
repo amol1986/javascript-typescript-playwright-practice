9726754705ca2cfac6965cd3196676b8df9ebe71
amol1986 <amolsureshpisal@gmail.com>
2026-09-09 13:14:42 +0530

    code with harry practice set chapter one added

---
+++ CodeWithHarry/Ch_01_practice.js
@@ -0,0 +1,26 @@
+console.log("Chapter One Practice Set");
+let number = "10";
+let b = 10;
+console.log(number + b);
+//ch01-Q02
+console.log(typeof (number + b));
+const a = {
+  name: "Harry",
+  section: 1,
+  isPrinciple: false,
+};
+console.log(a);
+
+a["friend"] = "shubam";
+console.log(a);
+
+const dictionary = {
+  disinterested: "unbiased by personal interest or advantage",
+  subterranean:
+    "existing, situated, or operating below the surface of the earth; underground.",
+  ebullient:
+    "overflowing with fervor, enthusiasm, or excitement; high-spirited.",
+};
+console.log(dictionary);
+console.log(dictionary.ebullient);
+console.log(dictionary["subterranean"]);
