48cdb5db7d3a73cc4511cb77ef7ec0146f757e76
amol1986 <amolsureshpisal@gmail.com>
2026-09-21 19:36:35 +0530

    Practice JavaScript object properties

---
+++ 01-javascript-basics/47-objects-basics.js
@@ -0,0 +1,9 @@
+const user = {
+  name: "Amol",
+  age: 41,
+  role: "QA Engineer",
+};
+
+console.log(user.name);
+console.log(user.age);
+console.log(user.role);
---
+++ 01-javascript-basics/48-object-properties.js
@@ -0,0 +1,13 @@
+const user = {
+  name: "Amol",
+  age: 41,
+};
+
+// Add a new property: role = "QA Engineer"
+user.role = "QA Engineer";
+
+// Update age to 42
+user.age = 42;
+delete user.age;
+
+console.log(user);
